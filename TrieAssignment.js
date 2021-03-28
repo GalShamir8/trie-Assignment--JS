c641348ddb10187a9b178ed36246be2e27f97e4c
Gal Shamir <galshamir8@gmail.com>
2021-03-28 20:39:09 +0300

    first commit

---
+++ TrieAssignment.js
@@ -0,0 +1,169 @@
+class Node {
+
+    constructor(val) {
+        this.val = val;
+        this.allNodes = [];
+    }
+
+    addNode(node) {
+        this.allNodes.push(node);
+    }
+
+}
+
+var wordException = "Invalid word";
+
+class Trie {
+
+
+    constructor(word) {
+        this.init(word);
+    }
+
+    init(word) {
+        if (!word || !!!word || word.length === 0)
+            throw wordException;
+        this.head = new Node(word.charAt(0));
+        word = word.substring(1);
+        if (word.length > 1) {
+            let newNode = new Node(word.charAt(0));
+
+            this.head.addNode(newNode);
+            this.addWord(newNode, word.substring(1));
+        } else if (!!!word) {
+            let newNode = new Node(word.charAt(0));
+            this.head.addNode(newNode);
+        }
+
+    }
+
+    insert(word) {
+        if (!this.checkValid(word))
+            throw wordException;
+
+        let flag = false;
+        word = word.substring(1);
+
+        if (word.length > 0) {
+            let lst = this.head.allNodes.filter(n => n.val === word.charAt(0));
+
+            if (lst.length > 0) {
+                this.handleInsert(lst[0], word.substring(1));
+                flag = true;
+            }
+        }
+        if (!flag) {
+            let newNode = new Node(word.charAt(0));
+            this.head.addNode(newNode);
+            this.addWord(newNode, word);
+        }
+    }
+
+
+    addWord(node, word) {
+        let currentNode = node;
+        while (word.length > 0) {
+            let nextNode = new Node(word.charAt(0));
+            currentNode.addNode(nextNode);
+            currentNode = nextNode;
+            word = word.substring(1);
+        }
+    }
+
+    handleInsert(node, word) {
+        //breaking condition
+        if (word.length === 1) {
+            if (node.allNodes.size > 0) {
+                let lst = this.node.allNodes.filter(n => n.val === word.charAt(0));
+                if (lst.length > 0)
+                    return;
+            }
+            let newNode = new Node(word.charAt(0));
+            node.addNode(newNode);
+            return;
+        }
+
+        let flag = false;
+
+        let lst = node.allNodes.filter(n => n.val === word.charAt(0));
+        if (lst.length > 0) {
+            flag = true;
+            this.handleInsert(lst[0], word.substring(1));
+        }
+
+        if (!flag) {
+            let newNode = new Node(word.charAt(0));
+            node.addNode(newNode);
+            this.addWord(newNode, word);
+        }
+    }
+
+    search(word) {
+        if (!this.checkValid(word))
+            throw wordException;
+
+        return this.searchWord(this.head, word);
+    }
+
+    searchWord(node, word) {
+        //breaking condition
+        if (word.length === 1) {
+            return word.charAt(0) === node.val;
+        }
+
+        if (word.charAt(0) === node.val) {
+            let lst = node.allNodes.filter(n => n.val === word.charAt(1));
+            if (lst.length > 0)
+                return this.searchWord(lst[0], word.substring(1));
+
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @param word
+     * @return
+     * @throws Exception
+     */
+    remove(word) {
+        if (!this.checkValid(word))
+            throw wordException;
+
+        let currentNode = this.head;
+        let branch = this.head;
+        let lastChar = ' ';
+        if (this.search(word)) {
+            word = word.substring(1);
+            while (word.length > 0) {
+                let lst = currentNode.allNodes.filter(n => n.val === word.charAt(0));
+                if (lst.length > 0) {
+                    if (currentNode.allNodes.length > 1) {
+                        branch = currentNode;
+                        lastChar = word.charAt(0);
+                    }
+                    currentNode = lst[0];
+                }
+                word = word.substring(1);
+            }
+        }
+        let lst = branch.allNodes.filter(n => n.val === lastChar);
+        if (lst.length > 0)
+            return branch.allNodes.splice(lst[0], 1);
+        return false;
+    }
+
+checkValid(word) {
+    return word && !!!word || !(word.length === 0) && this.head.val === word.charAt(0);
+}
+}
+
+t = new Trie("Amit");
+t.insert("Amir");
+t.search("Amil")
+console.log("Amir exist? " + t.search("Amir") + "\nAmit exist?" + t.search("Amit") +
+    "\nAmil exist? " + t.search("Amil"));
+t.remove("Amir");
+console.log("Amir exist? " + t.search("Amir") + "\nAmit exist? " + t.search("Amit") +
+    "\nAmil exist? " + t.search("Amil"));
+
